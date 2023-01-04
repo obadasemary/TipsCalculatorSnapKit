30acd3d5579d001fa25a74aab2795d442a9d4811
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-04 17:43:59 +0300

    Initial Commit

---
+++ TipsCalculatorSnapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipsCalculatorSnapKit
+//
+//  Created by Abdelrahman Mohamed on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
