71e160b96cee9033342283c485fa7b38214154e1
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-05 04:14:43 +0300

    Add Theme Font

--- TipsCalculatorSnapKit/Config/ThemeFont.swift
+++ TipsCalculatorSnapKit/Config/ThemeFont.swift
@@ -5,4 +5,19 @@
 //  Created by Abdelrahman Mohamed on 5.01.2023.
 //
 
-import Foundation
+import UIKit
+
+struct ThemeFont {
+
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
