bc7bb5edb51b035cc1f7060b5f31d99a31410a0f
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-05 04:14:33 +0300

    Add ThemeColor

--- TipsCalculatorSnapKit/Config/ThemeColor.swift
+++ TipsCalculatorSnapKit/Config/ThemeColor.swift
@@ -5,4 +5,14 @@
 //  Created by Abdelrahman Mohamed on 5.01.2023.
 //
 
-import Foundation
+import UIKit
+
+struct ThemeColor {
+
+    static let backgroundColor = UIColor(hexString: "8ECAE6")
+    static let primaryColor = UIColor(hexString: "FB8500")
+    static let secondaryColor = UIColor(hexString: "FFB703")
+    static let textColor = UIColor(hexString: "023047")
+    static let separatorColor = UIColor(hexString: "219EBC")
+    
+}
