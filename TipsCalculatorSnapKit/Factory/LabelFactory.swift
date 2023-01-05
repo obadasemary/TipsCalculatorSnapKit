13c6f29e0df927203f74a502b85d0d3fde52ec07
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-05 04:15:15 +0300

    Add Label Factory

--- TipsCalculatorSnapKit/Factory/LabelFactory.swift
+++ TipsCalculatorSnapKit/Factory/LabelFactory.swift
@@ -5,4 +5,24 @@
 //  Created by Abdelrahman Mohamed on 5.01.2023.
 //
 
-import Foundation
+import UIKit
+
+struct LabelFactory {
+
+    static func build(
+        text: String?,
+        font: UIFont,
+        backgroundColor: UIColor = .clear,
+        textColor: UIColor = ThemeColor.textColor,
+        textAlignment: NSTextAlignment = .center
+    ) -> UILabel {
+
+        let label = UILabel()
+        label.text = text
+        label.font = font
+        label.backgroundColor = backgroundColor
+        label.textColor = textColor
+        label.textAlignment = textAlignment
+        return label
+    }
+}
