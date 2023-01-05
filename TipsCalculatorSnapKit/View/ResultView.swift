89e35d7ede95ca6b09c9ca02a7afd9216c8f7eab
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-05 04:16:13 +0300

    Add Dummy Result View

--- TipsCalculatorSnapKit/View/ResultView.swift
+++ TipsCalculatorSnapKit/View/ResultView.swift
@@ -5,4 +5,20 @@
 //  Created by Abdelrahman Mohamed on 4.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class ResultView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func layout() {
+        backgroundColor = .gray
+    }
+}
