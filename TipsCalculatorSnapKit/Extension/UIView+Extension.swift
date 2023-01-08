29d6f128bb650b77800b8cdbc1603c4f159700e4
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-09 00:12:12 +0300

    Add UIView Extension

--- TipsCalculatorSnapKit/Extension/UIView+Extension.swift
+++ TipsCalculatorSnapKit/Extension/UIView+Extension.swift
@@ -5,4 +5,25 @@
 //  Created by Abdelrahman Mohamed on 9.01.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+
+    func addShadow(
+        offset: CGSize,
+        color: UIColor,
+        radius: CGFloat,
+        opacity: Float
+    ) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+}
