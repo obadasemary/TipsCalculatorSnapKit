6b364083bf56eb372fcc732d423bc54d369b02c8
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-09 00:12:27 +0300

    Add Amount View

--- TipsCalculatorSnapKit/View/AmountView.swift
+++ TipsCalculatorSnapKit/View/AmountView.swift
@@ -5,4 +5,20 @@
 //  Created by Abdelrahman Mohamed on 8.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class AmountView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func layout() {
+        backgroundColor = .red
+    }
+}
