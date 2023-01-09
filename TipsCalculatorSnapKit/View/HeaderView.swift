3f43ec578a8b62f73824389c386691b1fc1182ef
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-09 03:25:33 +0300

    implement Header View

--- TipsCalculatorSnapKit/View/HeaderView.swift
+++ TipsCalculatorSnapKit/View/HeaderView.swift
@@ -5,4 +5,65 @@
 //  Created by Abdelrahman Mohamed on 9.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class HeaderView: UIView {
+
+    private let topLabel: UILabel = {
+        LabelFactory.build(
+            text: nil,
+            font: ThemeFont.bold(ofSize: 18)
+        )
+    }()
+
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: nil,
+            font: ThemeFont.regular(ofSize: 16)
+        )
+    }()
+
+    private let topSpacerView = UIView()
+    private let bottomSpacerView = UIView()
+
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(
+            arrangedSubviews: [
+                topSpacerView,
+                topLabel,
+                bottomLabel,
+                bottomSpacerView
+            ]
+        )
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = -4
+        return stackView
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func layout() {
+        addSubview(stackView)
+
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        topSpacerView.snp.makeConstraints { make in
+            make.height.equalTo(bottomSpacerView)
+        }
+    }
+
+    func configure(topText: String, bottomText: String) {
+        topLabel.text = topText
+        bottomLabel.text = bottomText
+    }
+}
