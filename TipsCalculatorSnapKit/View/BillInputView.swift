6113840b791a2e9fdb55de54e391c78282d134cf
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-01-09 03:26:49 +0300

    implement Bill Input View

--- TipsCalculatorSnapKit/View/BillInputView.swift
+++ TipsCalculatorSnapKit/View/BillInputView.swift
@@ -9,6 +9,69 @@ import UIKit
 
 class BillInputView: UIView {
 
+    private let headerView: HeaderView = {
+        let view = HeaderView()
+        view.configure(
+            topText: "Enter",
+            bottomText: "your bill"
+        )
+        return view
+    }()
+
+    let textFieldContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addCornerRadius(radius: 8.0)
+        return view
+    }()
+
+    private let currencyDenominationLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: "$",
+            font: ThemeFont.bold(ofSize: 24)
+        )
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return label
+    }()
+
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.font = ThemeFont.demibold(ofSize: 28)
+        textField.keyboardType = .decimalPad
+        textField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        textField.tintColor = ThemeColor.textColor
+        textField.textColor = ThemeColor.textColor
+        // Add toolbar
+        let toolBar = UIToolbar(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: frame.size.width,
+                height: 36
+            )
+        )
+        toolBar.barStyle = .default
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(
+            title: "Done",
+            style: .plain,
+            target: self,
+            action: #selector(doneButtonTapped)
+        )
+        toolBar.items = [
+            UIBarButtonItem(
+                barButtonSystemItem: .flexibleSpace,
+                target: nil,
+                action: nil
+            ),
+            doneButton
+        ]
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+        return textField
+    }()
+
     init() {
         super.init(frame: .zero)
         layout()
@@ -19,6 +82,35 @@ class BillInputView: UIView {
     }
 
     func layout() {
-        backgroundColor = .green
+        [headerView, textFieldContainerView].forEach(addSubview(_:))
+
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(textFieldContainerView.snp.centerY)
+            make.width.equalTo(68)
+            make.trailing.equalTo(textFieldContainerView.snp.leading).offset(-24)
+        }
+
+        textFieldContainerView.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview()
+        }
+
+        textFieldContainerView.addSubview(currencyDenominationLabel)
+        textFieldContainerView.addSubview(textField)
+
+        currencyDenominationLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(textFieldContainerView.snp.leading).offset(16)
+        }
+
+        textField.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(currencyDenominationLabel.snp.trailing).offset(16)
+            make.trailing.equalTo(textFieldContainerView.snp.trailing).offset(-16)
+        }
+    }
+
+    @objc private func doneButtonTapped() {
+        textField.endEditing(true)
     }
 }
